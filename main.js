5c419d70aced58411b6a4fcb865fdde524850844
Vitor <108533713+Vitormatheusgrahl@users.noreply.github.com>
2024-06-20 12:00:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
+
+const perguntas =  [
+    {
+        enunciado: "Assim que saiu da escola você se depara com uma tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        alternativas: [
+            "Isso é assustador!",
+            "Isso é maravilhoso!"
+        ] 
+    },
+
+    {
+        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial (IA), uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre IA em sala de aula. Qual atitude você toma?",
+        alternativas: [
+            "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+            "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema."
+        ]
+    },
+
+    {
+        enunciado: "Após a elaboração do trabalho, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativa: [
+            "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+            "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."
+        ]
+    },
+
+    {
+        enunciado: "Ao final da discussão, vocẽ precisou criar uma imagem no computador que reprensentasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+            "Criar uma imagem utilizando um gerador de imagem de IA."
+        ]
+    },
+
+    {
+        enunciado: "Vocẽ tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda de uma IA. O problema é que está totalmente igual ao chat. O que vocẽ faz?",
+        alternativas: [
+            "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é uma problema utilizar o texto inteiro.",
+            "O chat pode ser uma tecnologia muito avançado, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
+        ]
+    }
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostrapergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent=perguntaAtual.enunciado;
+    mostrapergunta();
+    }
+
+    function mostraAlternativa(){
+        for (const alternativa of perguntaAtual.alternativas){
+            const botaoAlternativas=document.createElement("button");
+            botaoAlternativas.textContent=alternativa;
+            caixaAlternativas.appendChild(botaoAlternativas)
+        }
+    }
+
+    mostrapergunta();
