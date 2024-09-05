68a903ca720068b1090968e31889e907bdb11ec2
Vitor <108533713+Vitormatheusgrahl@users.noreply.github.com>
2024-09-05 10:50:08 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,169 @@
+export const perguntas = [
+    {
+        enunciado: "Quantos jogadores há em cada equipe no futebol de areia?",
+        alternativas: [
+            {
+                texto: "A) 5 jogadores",
+                afirmacao: [
+                    "A) 5 jogadores: Correto. Entendimento correto do formato do time.",
+                    "B) 7 jogadores: Incorreto. Pode levar à confusão sobre o número de jogadores em campo e a dinâmica do jogo."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "B) 7 jogadores",
+                afirmacao: [
+                    "A) 10 minutos: Incorreto. Pode resultar em um mal-entendido sobre o tempo de jogo e a estratégia.",
+                    "B) 12 minutos: Correto. Compreensão precisa da estrutura do jogo."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é a duração de cada período em uma partida de futebol de areia?",
+        alternativas: [
+            {
+                texto: "A) 10 minutos ",
+                afirmacao: [
+                    "A) Gramado: Incorreto. Ignora a natureza da superfície e as especificidades do jogo.",
+                    "B) Areia: Correto. Entendimento da superfície fundamental para o esporte.",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "B) 12 minutos",
+                afirmacao: [
+                    "A) 2 períodos: Incorreto. Pode causar confusão sobre a duração e o gerenciamento do jogo.",
+                    "B) 3 períodos: Correto. Entendimento adequado da estrutura do jogo.",
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "O futebol de areia é jogado em qual tipo de superfície?",
+        alternativas: [
+            {
+                texto: "A) Gramado",
+                afirmacao: [
+                    "A) 20x25 metros: Incorreto. Pode levar a uma percepção inadequada das dimensões do campo.",
+                    "B) 35x40 metros: Correto. Compreensão correta do espaço de jogo.",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "B) Areia",
+                afirmacao: [
+                    "A) Sim, para todos os jogadores: Incorreto. Entendimento incorreto das regras, afetando a percepção das funções dos jogadores.",
+                    "B) Não, apenas o goleiro pode usar as mãos: Correto. Compreensão das regras sobre o uso das mãos.",
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Quantos períodos há em uma partida de futebol de areia?",
+        alternativas: [
+            {
+                texto: "A) 2 períodos",
+                afirmacao: [
+                    "A) Substituições ilimitadas durante o jogo: Correto. Entendimento correto da flexibilidade nas substituições.",
+                    "B) Apenas uma substituição por período: Incorreto. Compreensão errada das regras de substituição."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "B) 3 períodos",
+                afirmacao: [
+                    "A) A bola deve passar completamente pela linha de gol: Correto. Compreensão precisa das regras de marcação de gols.",
+                    "B) A bola deve tocar o travessão e cair dentro da área: Incorreto. Desconhecimento da regra de validação de gols.",
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é o tamanho aproximado da quadra de futebol de areia?",
+        alternativas: [
+            {
+                texto: "A) 20x25 metros",
+                afirmacao: [
+                    "A) Cartões amarelos e vermelhos: Incorreto. Pode levar a uma compreensão errada das penalidades.",
+                    "B) Tiros livres ou penas de 2 minutos: Correto. Compreensão das penalidades e suas consequências."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "B) 35x40 metros",
+                afirmacao: [
+                    " A) Uniformes completos com tênis: Incorreto. Pode indicar um entendimento incorreto das necessidades de vestuário para o esporte.",
+                    "B) Roupas leves como trajes de banho: Correto. Compreensão apropriada do vestuário adequado para o ambiente de areia."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "É permitido o uso das mãos no futebol de areia?",
+        alternativas: [
+            {
+                texto: "A) Sim, para todos os jogadores",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "B) Não, apenas o goleiro pode usar as mãos",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Como são realizadas as substituições no futebol de areia?",
+        alternativas: [
+            {
+                texto: "A) Substituições ilimitadas durante o jogo",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "B) Apenas uma substituição por período",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é a forma de marcar um gol no futebol de areia?",
+        alternativas: [
+            {
+                texto: "A) A bola deve passar completamente pela linha de gol",
+                afirmacao: [
+                    "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.",
+                    "Ainda acha que os meios de desenho tradicionais são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer pintura em aquarela."
+                ],
+            },
+            {
+                texto: "B) A bola deve tocar o travessão e cair dentro da área",
+                afirmacao: [
+                    "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!",
+                    "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
+                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a IA é capaz de empoderar essas pessoas a tirarem ideias do papel."
+                ],
+            },
+        ]
+    }
+];
